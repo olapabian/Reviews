31d24e02a4f3c26b258af98b7325ef796c64b0d5
Ola <ola.pabian@interia.com>
2023-12-14 11:48:41 +0100

    deleting review

--- src/main/java/com/example/Reviews/Controllers/MyReviewControllers/EditReviewController.java
+++ src/main/java/com/example/Reviews/Controllers/MyReviewControllers/EditReviewController.java
@@ -2,35 +2,21 @@ package com.example.Reviews.Controllers.MyReviewControllers;
 
 import com.example.Reviews.Model.Review;
 import com.example.Reviews.Repositories.ReviewRepository;
-import jakarta.servlet.http.HttpSession;
-import jdk.dynalink.linker.LinkerServices;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
 import java.util.Optional;
+
 @AllArgsConstructor
 @Controller
-@RequestMapping("/myReview")
-public class MyReviewController {
-    ReviewRepository reviewRepository;
-    @GetMapping
-    public String MyReviewPage(@RequestParam("id") Long id, Model model) {
-        Optional<Review> reviewOptional = reviewRepository.findById(id);
-        if(reviewOptional.isPresent()){
-            Review review = reviewOptional.get();
-            model.addAttribute("review",review);
-        }
-
-        return "/MyReviewPages/myReviewPage";
-    }
-    @PostMapping("/deleteReview")
-    public String deleteReview(@RequestParam("id") Long id, Model model) {
+public class EditReviewController {
+    private final ReviewRepository reviewRepository;
+    @GetMapping("/editReview")
+    public String editReview(@RequestParam("id") Long id, Model model) {
         if (id != null){
             Optional<Review> reviewOptional = reviewRepository.findById(id);
             if(reviewOptional.isPresent()){
